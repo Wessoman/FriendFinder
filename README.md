29661360355e3cf0a28539ff8cb1c9c4ace535d3
Antony Hernandez <wessoman@hotmail.com>
2019-07-09 08:56:08 -0500

    Ver .04 Java Engine first try

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ A wonderful attempt at a friend-finding/Dating app
 # VersionHistory
 
 .01-.03 File System, initial planning and commits. 
-
+.04-05 Added API linkup files, Javascript builds.
--- server.js
+++ server.js
@@ -28,6 +28,7 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 // ROUTER
 // The below points our server to a series of "route" files.
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// Note that the API routes are in a seperate folder invisible to the user
 // ================================================================================
 
 require("./app/routing/apiRoutes")(app);
